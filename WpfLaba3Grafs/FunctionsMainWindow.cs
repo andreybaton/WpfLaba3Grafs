96e861958a652075e640056443122429f2579153
Андрей <andreybaton63@gmail.com>
2024-11-02 22:49:46 +0400

    Доработка интерфейса под ТЗ

--- WpfLaba3Grafs/FunctionsMainWindow.cs
+++ WpfLaba3Grafs/FunctionsMainWindow.cs
@@ -15,53 +15,53 @@ namespace WpfLaba3Grafs
 {
     public class FunctionsMainWindow : MainWindow
     {
-        public bool newVertex = false;
-        public bool newEdge = false;
+        //public bool newVertex = false;
+        //public bool newEdge = false;
         
-        private void CreateVertex(Point position)
-        {
-            Ellipse vertex = new Ellipse()
-            {
-                Width = 20,
-                Height = 20,
-                Stroke = Brushes.Black,
-                StrokeThickness = 2
-            };
-            Canvas.SetLeft(vertex, position.X - vertex.Width / 2);
-            Canvas.SetTop(vertex, position.Y - vertex.Height / 2);
+        //private void CreateVertex(Point position)
+        //{
+        //    Ellipse vertex = new Ellipse()
+        //    {
+        //        Width = 20,
+        //        Height = 20,
+        //        Stroke = Brushes.Black,
+        //        StrokeThickness = 2
+        //    };
+        //    Canvas.SetLeft(vertex, position.X - vertex.Width / 2);
+        //    Canvas.SetTop(vertex, position.Y - vertex.Height / 2);
 
-            DrawingCanvas.Children.Add(vertex);
-        }
+        //    DrawingCanvas.Children.Add(vertex);
+        //}
         
-        private void CreateEdge(Point pos1, Point pos2) 
-        {
-            Line edge = new Line()
-            {
-                X1 = pos1.X,
-                Y1 = pos1.Y,
-                X2 = pos2.X,
-                Y2 = pos2.Y,
-                Stroke = Brushes.Black,
-                StrokeThickness = 2
-            };
-            DrawingCanvas.Children.Add(edge);
-        }
-        public void OnMouseLeftBtnDown_DrawingGraph (object sender, MouseButtonEventArgs e)
-        {
-            Point mousePosition = e.GetPosition(DrawingCanvas);
-            MessageBox.Show(mousePosition.ToString());
-            MessageBox.Show(newVertex.ToString());
-            if (newVertex)
-            {
-                CreateVertex(mousePosition);
-                newVertex = false;
-            }
-            else if (newEdge)
-            {
-                Point secondMousePos = e.GetPosition(DrawingCanvas);
-                CreateEdge(mousePosition, secondMousePos);
-                newEdge = false;
-            }
-        }
+        //private void CreateEdge(Point pos1, Point pos2) 
+        //{
+        //    Line edge = new Line()
+        //    {
+        //        X1 = pos1.X,
+        //        Y1 = pos1.Y,
+        //        X2 = pos2.X,
+        //        Y2 = pos2.Y,
+        //        Stroke = Brushes.Black,
+        //        StrokeThickness = 2
+        //    };
+        //    DrawingCanvas.Children.Add(edge);
+        //}
+        //public void OnMouseLeftBtnDown_DrawingGraph (object sender, MouseButtonEventArgs e)
+        //{
+        //    Point mousePosition = e.GetPosition(DrawingCanvas);
+        //    MessageBox.Show(mousePosition.ToString());
+        //    MessageBox.Show(newVertex.ToString());
+        //    if (newVertex)
+        //    {
+        //        CreateVertex(mousePosition);
+        //        newVertex = false;
+        //    }
+        //    else if (newEdge)
+        //    {
+        //        Point secondMousePos = e.GetPosition(DrawingCanvas);
+        //        CreateEdge(mousePosition, secondMousePos);
+        //        newEdge = false;
+        //    }
+        //}
     }
 }
